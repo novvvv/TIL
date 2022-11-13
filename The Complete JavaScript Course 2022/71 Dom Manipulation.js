afc285c02c63b6c487205cd6be2e26e554b3fc39
nov <85823762+novvvv@users.noreply.github.com>
2022-11-13 15:01:19 +0900

    Create 71 Dom Manipulation.js

---
+++ The Complete JavaScript Course 2022/71 Dom Manipulation.js
@@ -0,0 +1,11 @@
+'use strict';
+// class selector .message
+// id selector #message
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '✨Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
