07f791086809b616d43746f46f02a204e6796782
nov <85823762+novvvv@users.noreply.github.com>
2022-11-27 17:17:08 +0900

    Create 006-destructingArray.js

---
+++ JavaScript-Study/006-destructingArray.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterManu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+// destructing Array
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructing assignement
+// 원본 데이터에는 영향을 미치지 않는다.
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const [frist, , second] = restaurant.categories;
+console.log(first, second);
+
+// 💡 switching variables
+let [main, , secondary] = restaurant.categories;
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
+
+// 💡 switchinig - reasigning value
+[main, secondary] = [secondary, main];
+
+console.log(restaurant.order(2, 0));
+// Return - Garlic Bread & Pizza
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+
+// nested Array with Destructing Array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
+// r = undefined
+const [x1, y1, z1 = 1] = [8, 9];
